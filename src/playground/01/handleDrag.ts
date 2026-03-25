613db4ab70a56a44c14c843db67c66692d822f8f
Hans <543048143@qq.com>
2026-03-25 11:39:28 +0800

    完善练习1
    完善类型收窄，让守卫更确切

--- src/playground/01/handleDrag.ts
+++ src/playground/01/handleDrag.ts
@@ -29,15 +29,33 @@ function isRecord(v: unknown): v is Record<string, unknown> {
     return typeof v === 'object' && v !== null
 }
 
-// TODO 2: 实现 isDressupItem
+/**
+  TODO 2: 实现 isDressupItem
+  补充：类型校验更加收窄
+*/ 
 function isDressupItem(v: unknown): v is DressupItem {
-    return isRecord(v) && 'dressup_manage_id' in v && 'dressup_type' in v
+    // return isRecord(v) && 'dressup_manage_id' in v && 'dressup_type' in v
+    return isRecord(v)&&
+    isStringOrNumber(v.dressup_manage_id)&&
+    isStringOrNumber(v.dressup_type)&&
+    isNumberOrUndefined(v.sort_num)&&
+    isBooleanOrUndefined(v.checked)
 }
 
+function isStringOrNumber(v:unknown): v is string | number {
+  return typeof v === 'string' || typeof v === 'number';
+}
+function isNumberOrUndefined(v:unknown): v is number | undefined {
+  return v === undefined || typeof v === 'number';
+}
+function isBooleanOrUndefined(v:unknown): v is boolean | undefined {
+  return  v === undefined || typeof v === 'boolean';
+}
 // TODO 3: 实现数组守卫
 function isDressupItemArray(v: unknown): v is DressupItem[] {
   return Array.isArray(v) && v.every(isDressupItem)
 }
+
 // TODO 4: 实现解析函数，返回 DragPayload | null
 function parseDragPayload(raw: string): DragPayload | null {
   const parsed = safeParse(raw)
